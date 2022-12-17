32a39b985dc924309531ff3a0b0fa443325a78c2
PiloteProdYT <69173006+PiloteProd@users.noreply.github.com>
2022-12-17 23:06:22 +0100

    Update v1.2

--- files/tetris.py
+++ files/tetris.py
@@ -108,3 +108,73 @@ def draw_tetromino(canvas, tetromino):
                 if tetromino.rotation == 0 and (i, j) in [(1, 2), (2, 2), (2, 1), (3, 1)]:
                     canvas.create_rectangle(tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE, tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE + BLOCK_SIZE,
                                            fill=tetromino.color)
+                elif tetromino.rotation == 1 and (i, j) in [(2, 1), (2, 2), (1, 2), (1, 3)]:
+                    canvas.create_rectangle(tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE, tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE,
+                                           tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE + BLOCK_SIZE, tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE + BLOCK_SIZE,
+                                           fill=tetromino.color)
+                elif tetromino.rotation == 2 and (i, j) in [(1, 1), (2, 1), (2, 2), (3, 2)]:
+                    canvas.create_rectangle(tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE, tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE,
+                                           tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE + BLOCK_SIZE, tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE + BLOCK_SIZE,
+                                           fill=tetromino.color)
+                elif tetromino.rotation == 3 and (i, j) in [(2, 3), (2, 2), (1, 2), (1, 1)]:
+                    canvas.create_rectangle(tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE, tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE,
+                                           tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE + BLOCK_SIZE, tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE + BLOCK_SIZE,
+                                           fill=tetromino.color)
+            elif tetromino.shape == 5:
+                if tetromino.rotation == 0 and (i, j) in [(1, 1), (2, 1), (3, 1), (3, 2)]:
+                    canvas.create_rectangle(tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE, tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE,
+                                           tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE + BLOCK_SIZE, tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE + BLOCK_SIZE,
+                                           fill=tetromino.color)
+                elif tetromino.rotation == 1 and (i, j) in [(2, 1), (2, 2), (2, 3), (1, 3)]:
+                    canvas.create_rectangle(tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE,tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE + BLOCK_SIZE,
+                                           fill=tetromino.color)
+                elif tetromino.rotation == 2 and (i, j) in [(1, 2), (2, 2), (3, 2), (1, 1)]:
+                    canvas.create_rectangle(tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE, tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE,
+                                           tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE + BLOCK_SIZE, tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE + BLOCK_SIZE,
+                                           fill=tetromino.color)
+                elif tetromino.rotation == 3 and (i, j) in [(2, 3), (2, 2), (2, 1), (3, 1)]:
+                    canvas.create_rectangle(tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE, tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE,
+                                           tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE + BLOCK_SIZE, tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE + BLOCK_SIZE,
+                                           fill=tetromino.color)
+            elif tetromino.shape == 6:
+                if tetromino.rotation == 0 and (i, j) in [(1, 1), (1, 2), (2, 2), (3, 2)]:
+                    canvas.create_rectangle(tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE, tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE,
+                                           tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE + BLOCK_SIZE, tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE + BLOCK_SIZE,
+                                           fill=tetromino.color)
+                elif tetromino.rotation == 1 and (i, j) in [(1, 1), (2, 1), (2, 2), (2, 3)]:
+                    canvas.create_rectangle(tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE, tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE,
+                                           tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE + BLOCK_SIZE, tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE + BLOCK_SIZE,
+                                           fill=tetromino.color)
+                elif tetromino.rotation == 2 and (i, j) in [(1, 2), (2, 2), (3, 2), (1, 1)]:
+                    canvas.create_rectangle(tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE,
+                                            tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE,
+                                            tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE + BLOCK_SIZE,
+                                            tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE + BLOCK_SIZE,
+                                            fill=tetromino.color)
+                elif tetromino.rotation == 3 and (i, j) in [(2, 3), (2, 2), (2, 1), (1, 1)]:
+                    canvas.create_rectangle(tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE, tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE,
+                                           tetromino.x * BLOCK_SIZE + j * BLOCK_SIZE + BLOCK_SIZE, tetromino.y * BLOCK_SIZE + i * BLOCK_SIZE + BLOCK_SIZE,
+                                           fill=tetromino.color)
+        # On vérifie si la pièce peut descendre
+        if not check_collision(tetromino, (tetromino.x, tetromino.y + 1), grid):
+            tetromino.y += 1
+        else:
+            # Si la pièce ne peut pas descendre, on la "fixe" sur la grille
+            for i in range(TETROMINO_SIZE):
+                for j in range(TETROMINO_SIZE):
+                    if tetromino.grid[i][j] == 1:
+                        grid[tetromino.y + i][tetromino.x + j] = tetromino.color
+            lines_cleared = clear_lines(grid)
+            score += lines_cleared
+            # On crée une nouvelle pièce
+            tetromino = Tetromino()
+            game_over = check_game_over(tetromino, grid)
+        root.after(1000, update)
+
+# On initialise la grille
+grid = create_grid()
+# On crée la première pièce
+tetromino = Tetromino()
+
+root.after(1000, update)
+root.mainloop()
